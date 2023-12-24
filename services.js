c0fc382cd462ced52ac7e0a6ac391ac16072c163
Onur Demiray <baycosinus@gmail.com>
2023-12-24 22:55:45 +0300

    Bugfix on services cards.

--- services.js
+++ services.js
@@ -2,29 +2,33 @@ function setInfoBoxes(){
     console.log("settingInfoBoxes");
     let infoboxes = Array.from(document.getElementsByClassName("infobox"));
 
-    infoboxes.forEach(element => {element.style.display = "none"; })
+    infoboxes.forEach(element => {element.style.display = "none"; element.parentElement.style.display = "none"; })
 }
 
 function handleClick(e) {
-    console.log("clicked!");
+    let currentlyActiveBox = null;
     let button = e.className.split(' ')[1]
     let infoboxes = Array.from(document.getElementsByClassName("infobox"));
     let infoBox = document.getElementsByClassName(button + "-content")[0];
+    let currentInfoBoxContainer = infoBox.parentElement;
 
     infoboxes.forEach(element => {
+        let parent = element.parentElement;
+
         if (!element.className.includes(button + "-content")) {
             element.style.display = "none";
         }
         else {
-            if(element.style.display === "none"){
+            if(element.style.display === "none" && parent.style.display === "none"){
                     element.style.animation = "slidedown 0.5s";
                     element.style.display = "block";
+
             }
             else{
                 element.style.display = "none";
             }
-
-            //element.style.display = element.style.display === "none" ? "block" : "none";
         }
+
+        currentInfoBoxContainer.style.display = infoBox.style.display;
     });
 }
