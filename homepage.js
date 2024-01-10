ecf20845807755ad0966d5a1506294b270c77849
Onur Demiray <baycosinus@gmail.com>
2024-01-10 21:56:13 +0300

    conflict fix and language selector recovery

--- homepage.js
+++ homepage.js
@@ -1,101 +1,22 @@
-function hamburgerOnClick() {
-    let mobileNavbar = document.getElementById("mobile-navbar");
-    if (!mobileNavbar) {
-    }
-
-    if (mobileNavbar.style.display == "none") {
-        mobileNavbar.style.display = "flex";
-    }
-    else {
-        mobileNavbar.style.display = "none"
-    }
-}
-
-async function firstLoad() {
-    const cookie = document.cookie;
-
-    const selects = document.getElementsByClassName("languageSelector");
-    let dict = await parseLang();
-    console.log(dict);
-    for (let select of selects) {
-        if (cookie != '') {
-            select.value = cookie.split('=')[1];
-        }
-        else {
-            select.value = "en";
-            document.cookie = "Lang=en";
-        }
-
-        if (dict && dict.languages && dict.languages.length > 0) {
-            dict.languages.forEach(x => {
-                var opt = document.createElement('option');
-                opt.value = x.short;
-                opt.innerHTML = x.name;
-                select.appendChild(opt);
-            });
-        }
-    }
-
-    //await setText();
-}
-
-function changelang(event) {
-    let target = event.target;
-    let value = target.value;
-    document.cookie = `Lang=${value}`;
-
-    setText();
-}
-
-async function parseLang() {
-    let dict = {};
-
-    await fetch('./dictionary.json')
-        .then((response) => response.json())
-        .then((json) => dict = json);
-
-    return dict;
-}
-
-async function setText() {
-    texts = Array.from(document.getElementsByClassName("text"));
-    let lang = document.cookie.split('=')[1];
-    let dict = await parseLang();
-
-    for (let i = 0; i < texts.length; i++) {
-        let id = texts[i].id;
-        let textDict = dict.texts.find((x) => x.id == id);
-        //console.log(id);
-        //console.log(textDict);
-        if (!textDict) {
-            continue;
-        }
-        let text = textDict[lang];
-        //console.log(text);
-        texts[i].innerHTML = text;
-    }
-}
-
-
-//#region HAMBURGER//
-
 var hamburgerMenuButton;
 var menuContainerMobile;
 var mobileNavbar;
 
-function onload() {
-    console.log("loaded");
-    hamburgerMenuButton = document.getElementById('hamburger-button');
+var dictionary;
+var currentLanguage;
+var languageBasedDictionary;
 
+async function onload() {
+    console.log("loaded");
+    await loadDictionary();
+    //await setLanguage();
+    await populateSelections();
+    hamburgerMenuButton = document.getElementById('hamburger-button');
     menuContainerMobile = document.querySelector('.menu-container-mobile');
     mobileNavbar = document.querySelector(".menu-container-mobile>nav");
-    console.log(mobileNavbar)
-    console.log(menuContainerMobile)
-    console.log(hamburgerMenuButton)
 }
 
 function handleHamburger() {
-    console.log(!hamburgerMenuButton.checked);
     if (!hamburgerMenuButton.checked) {
         menuContainerMobile.classList.remove("off");
         menuContainerMobile.classList.add("on");
@@ -107,8 +28,6 @@ function handleHamburger() {
         mobileNavbar.style.transition = "opacity 0.6s"
         mobileNavbar.style.opacity = "0";
     }
-
-    console.log(!hamburgerMenuButton.checked);
 }
 
 //#endregion HAMBURGER//
@@ -132,4 +51,75 @@ function handleScroll(amount) {
     if (gallerySlider) {
         gallerySlider.scrollLeft += amount * 2 * (gallerySliderWidth / galleryChildCount);
     }
+}
+
+
+
+// TRANSLATION
+async function loadDictionary() {
+    try {
+        const response = await fetch('./dictionary.json');
+        if (!response.ok) {
+            throw new Error(`Failed to load dictionary.json (HTTP ${response.status})`);
+        }
+        dictionary = await response.json();
+
+    } catch (error) {
+        console.error('Error loading dictionary:', error.message);
+    }
+}
+
+async function setLanguage() {
+    await loadDictionary();
+
+    var cookie = document.cookie;
+    console.log("cookie: " + cookie);
+    if (cookie === undefined || cookie.length === 0) {
+        document.cookie = "lang=tr";
+        currentLanguage = "tr";
+        console.log("cookie set to eng");
+    }
+    else {
+        currentLanguage = cookie.split('=')[1];
+    }
+    console.log("CR: " + currentLanguage)
+    const languageBasedDictionary = dictionary.texts.map(({ id, [currentLanguage]: langText }) => ({ id, text: langText }));
+    console.log(languageBasedDictionary);
+
+    languageBasedDictionary.forEach(element => {
+        console.log(element.id + ">" + element.text);
+        document.getElementById(element.id).innerHTML = element.text;
+    });
+}
+
+async function populateSelections(){
+    var desktopSelect = document.querySelector(".language-selector select");
+    var mobileSelect = document.querySelector(".language-selector-mobile select");
+    
+    desktopSelect.innerHtml = "";
+    mobileSelect.innerHTML = "";
+
+    dictionary.languages.forEach(lang => {
+        let option = document.createElement("option");
+        let option_mobile = document.createElement("option");
+        option.value = lang.short;
+        option.textContent = lang.name;
+
+        option_mobile.value = lang.short;
+        option_mobile.textContent = lang.name;
+
+        desktopSelect.appendChild(option)
+        mobileSelect.appendChild(option_mobile);
+    })
+
+    desktopSelect.value = currentLanguage;
+    mobileSelect.value = currentLanguage;
+}
+
+async function handleLanguageSelector(event){
+    var changedSelect = event.target;
+
+    document.cookie = "lang=" + changedSelect.value;
+    console.log(document.cookie);
+    await setLanguage();
 }
